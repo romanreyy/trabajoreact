ae67428c4810f10bf58dbd794b2823ff575dd8f4
Roman Rey <romanrey2006@gmail.com>
2023-09-26 23:10:39 -0300

    "nuevo commit"

--- src/Conteiners/Do_Post.js
+++ src/Conteiners/Do_Post.js
@@ -3,7 +3,12 @@ export default function CreatePost (){
     return (
             <div>
                 <h1>Crea tu Post</h1>
-
+                <br/>
+                <input type="text" placeholder="Titulo"/>
+                <br/>
+                <textarea/>
+                <br/>
+                <input type="submit" value="Postear"/>
             </div>
     );
 }
