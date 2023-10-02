686eb501830114b6f6cf6834d1ada2db70f7ef45
romanreyy <130393367+romanreyy@users.noreply.github.com>
2023-10-02 14:10:43 -0300

    Update Do_Post.js

--- src/Conteiners/Do_Post.js
+++ src/Conteiners/Do_Post.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+//import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import {Icon} from "../components/icon";
 import {SearchBar} from "../components/search_bar";
