628f5e2bfc8e4a00e65b95e8a2ed6b06c9611399
romanreyy <130393367+romanreyy@users.noreply.github.com>
2023-10-06 10:43:36 -0300

    Update Main.js

--- src/Conteiners/Main.js
+++ src/Conteiners/Main.js
@@ -1,40 +1,10 @@
 import React from "react";
 import '../App.css';
-import {SearchBar} from '../components/search_bar';
-import {Icon} from '../components/icon';
-import {ButtonCreate} from '../components/create_post';
-import {Login} from '../components/login';
 import {Posts} from '../components/posts';
-import {Link} from "react-router-dom";
-import CreatePost from './Do_Post';
-import {useState} from 'react';
-
 
 export default function Main() {
-
-    const [datos, setDatos] = useState([]);
-
-  const handleFormSubmit = (formData) => {
-    setDatos([...datos, formData]);
-  };
-
     return (
         <div>
-            <nav>
-                <Link to="/">
-                    <Icon/>
-                </Link>
-                <br/>
-                <hr/>
-                <SearchBar/>
-                <Link to="/CreatePost">
-                    <ButtonCreate/>
-                </Link>
-                <Link to="/LogIn">
-                    <Login/>
-                </Link>
-                <hr/>
-            </nav>
             <div
                 style={{
                     padding: "20px"
