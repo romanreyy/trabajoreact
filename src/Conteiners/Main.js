dd53cb78d63c17944d26be33c2ed847c5e93e60d
Roman Rey <romanrey2006@gmail.com>
2023-10-09 22:51:41 -0300

    "mejor manejo de dinamismo en la pagina"

--- src/Conteiners/Main.js
+++ src/Conteiners/Main.js
@@ -1,21 +1,27 @@
 import React from "react";
 import '../App.css';
 import {Posts} from '../components/Posts';
+import {PostDomain} from "../Domain/Post";
 
 export default function Main() {
+    const postList = getPost()
     return (
         <div>
             <div className="showPosts">
-                    <Posts/>
-                    <Posts/>
-                    <Posts/>
-                    <Posts/>
-                    <Posts/>
-                    <Posts/>
-                    <Posts/>
+                {postList.map((object, i) => <Posts post={object}/>)}
             </div>
 
         </div>
 
     );
 }
+
+const getPost = () => {
+    const usePost = [];
+    usePost.push(new PostDomain("hola argentina", "contenido 0", "12/08"))
+    usePost.push(new PostDomain("hola barca", "contenido 4", "11/09"))
+    usePost.push(new PostDomain("hola inter", "contenido 5", "30/04"))
+    usePost.push(new PostDomain("hola hola", "contenido 10", "28/04"))
+
+    return usePost;
+};
---
+++ src/Domain/Post.js
@@ -0,0 +1,7 @@
+ export class PostDomain {
+    constructor(title, description, date) {
+        this.title=title;
+        this.description=description;
+        this.date=date;
+    }
+}
